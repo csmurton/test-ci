3b9be0a6b00bf8de1d4843338d94b1bcf86f66a5
cmurton <cmurton@opendns.com>
2017-11-01 16:24:39 +0000

    Skip remote tests if endpoint isn't specified

--- test/sayHelloRemote.js
+++ test/sayHelloRemote.js
@@ -4,17 +4,21 @@ const request = require('request');
 
 describe('Hello World function (Remote)', function() {
     it('Hello World function should say... hello', function(done) {
-	var url = process.env.HELLO_API_ENDPOINT + "/hello";
-	console.log(url);
+	if(process.env.HELLO_API_ENDPOINT) {
+		var url = process.env.HELLO_API_ENDPOINT + "/hello";
 
-	request.get({ 'url': url }, function (err, result, body) {
-		if(err) {
-			throw new Error("Unable to say hello remotely: " + err);
-		}
+		request.get({ 'url': url }, function (err, result, body) {
+			if(err) {
+				throw new Error("Unable to say hello remotely: " + err);
+			}
 
-		assert.equal(result.statusCode, 200);
-		assert.equal(body, "Hello world 22");
+			assert.equal(result.statusCode, 200);
+			assert.equal(body, "Hello world 22");
+			done();
+		});
+	} else {
+		this.skip();
 		done();
-	});
+	}
     });
 });
